ffb3497a267cc3cbf33a1b95024614f2d8bb19f3
Jonathan Ruppel <jonathan_ruppel@yahoo.com>
2026-07-05 16:13:02 -0500

    adding postive int checks and sum logic

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if (Number.isInteger(num1) && num1>0 && Number.isInteger(num2) && num2>0)
+    {
+        let sum = 0
+        if (num1 < num2)
+        {
+            for (i = num1; i <= num2; i++)
+            {
+                sum+=i
+            }
+        }
+        else if (num1 > num2)
+        {
+            for (i = num2; i <= num1; i++)
+            {
+                sum+=i
+            }
+        }
+        return sum
+    }
+    else
+    {
+        return `'ERROR'`
+    }
 };
 
 // Do not edit below this line
